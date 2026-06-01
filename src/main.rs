62e435e0d66f001bd98d58eeba844b23704f0755
0xMush <mushaibali7t1@gmail.com>
2026-05-31 21:06:38 -0400

    Initial commit: LLM chat CLI app Colored

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use serde_json::{json, Value};
+use reqwest;
+use std::io;
+use colored::*;
+use dotenvy::dotenv;
+use std::env;
+
+
+
+#[tokio::main]
+async fn main() {
+
+    loop{
+        dotenv().expect(".env file not found");
+
+
+        let model = env::var("LLM_MODEL").expect("input a model in .env");
+
+        // prompt input
+        println!("--Enter Your Prompt--");
+        let mut prompt = String::new();
+        io::stdin().read_line(&mut prompt).expect("expected string");
+        println!("User asked : {}\n", prompt.red());
+
+        let api_key = env::var("API_KEY").expect("Input openroute key in .env file");
+
+        let payload = json!({
+        "model": model,
+        "messages": [{"role": "user", "content": prompt,}]
+        });
+        
+        let client = reqwest::Client::new();
+        let res = client.post("https://openrouter.ai/api/v1/chat/completions")
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&payload)
+        .send()
+        .await;
+
+        match res{
+            Ok(response) => {
+                let text = response.text().await.unwrap();
+                let v: Value = serde_json::from_str(&text).expect("REASON");
+                let content = v["choices"][0]["message"]["content"].as_str().unwrap();
+                println!("Response: {}\n", content.green());
+            },
+            Err(e) => println!("{}",e),
+        }
+    }
+}
